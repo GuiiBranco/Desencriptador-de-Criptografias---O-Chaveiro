42c63fed548dbb4a0a8a61539a978416a92d58eb
Guilherme Branco Rodrigues <151711519+GuiiBranco@users.noreply.github.com>
2024-08-21 23:22:45 +0000

    Criando o tradutor de morse

---
+++ js/descriptografando.js
@@ -0,0 +1,21 @@
+let msg = "..-. .- .-.. .- .--. --- ...- ---";
+
+const tabelaMorse = [
+    {codigo: '.-', letra: 'a'},     {codigo: '-...', letra: 'b'},
+    {codigo: '-.-.', letra: 'c'},   {codigo: '-..', letra: 'd'},
+    {codigo: '.', letra: 'e'},      {codigo: '..-.', letra: 'f'},
+    {codigo: '--.', letra: 'g'},    {codigo: '....', letra: 'h'},
+    {codigo: '..', letra: 'i'},     {codigo: '.---', letra: 'j'},
+    {codigo: '-.-', letra: 'k'},    {codigo: '.-..', letra: 'l'},
+    {codigo: '--', letra: 'm'},     {codigo: '-.', letra: 'n'},
+    {codigo: '---', letra: 'o'},    {codigo: '.--.', letra: 'p'},
+    {codigo: '--.-', letra: 'q'},   {codigo: '.-.', letra: 'r'},
+    {codigo: '...', letra: 's'},    {codigo: '-', letra: 't'},
+    {codigo: '..-', letra: 'u'},    {codigo: '...-', letra: 'v'},
+    {codigo: '.--', letra: 'w'},    {codigo: '-..-', letra: 'x'},
+    {codigo: '-.--', letra: 'y'},   {codigo: '--..', letra: 'z'}
+];
+
+function traduzirMorse(mensagem) {
+    
+};
